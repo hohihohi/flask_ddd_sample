3569d374ffe0548eb0724a54445b3545a40d83a9
hohihohi <a56.tazima@gmail.com>
2019-02-08 10:07:48 +0900

    fix bug: modfiy pipenv version

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.7.2
 
 WORKDIR /usr/local/app
 ADD Pipfile.lock .
-RUN pip install pipenv==11.10.4 && \
+RUN pip install pipenv==2018.11.26 && \
     pipenv install --ignore-pipfile -d
 
 ENTRYPOINT ["pipenv", "run"]
--- app.py
+++ app.py
@@ -1,14 +1,20 @@
+from flask import Flask
+
 from fds.domains import DataSource
 
 
-def main():
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
     data_source = DataSource("xxx12345", "test", "RAW", "west")
     print(f"data_source:{data_source.__dict__}")
     copied = data_source.copy()
     print(f"copied:{copied.__dict__}")
     print(f"data_source.obj:{data_source.object.__dict__}")
     print(f"data_source.obj:{copied.object.__dict__}")
+    return "Hello, Flask!"
 
 
 if __name__ == '__main__':
-    main()
+    app.run(host="0.0.0.0", port=80)
