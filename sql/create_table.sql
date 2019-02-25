2b9a6c46c484e79c9482b431963a039ffebc8064
hohihohi <a56.tazima@gmail.com>
2019-02-25 11:58:56 +0900

    modify database tables

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -12,18 +12,6 @@ CREATE TABLE regions (
 
 INSERT INTO regions (name) VALUES ('jp-east-1');
 
-CREATE TABLE data (
-    id         INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    user_id    VARCHAR(16) NOT NULL,
-    name       VARCHAR(32) NOT NULL,
-    data_type  VARCHAR(16) NOT NULL,
-    created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-    updated_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-    deleted_at DATETIME(6) NOT NULL DEFAULT '9999-12-31 23:59:59',
-    UNIQUE unq_user_id_name (user_id, name, deleted_at),
-    CONSTRAINT data_type_fk FOREIGN KEY (data_type) REFERENCES data_types (name) ON UPDATE CASCADE
-) Engine=InnoDB CHARSET=utf8mb4;
-
 CREATE TABLE buckets(
     id         INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     user_id    VARCHAR(16) NOT NULL,
@@ -42,14 +30,25 @@ CREATE TABLE objects (
     name       VARCHAR(32) NOT NULL,
     region     VARCHAR(16) NOT NULL,
     version    VARCHAR(32) NOT NULL,
-    data_id    INT UNSIGNED NOT NULL,
     bucket_id  INT UNSIGNED NOT NULL,
     created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
     updated_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     deleted_at DATETIME(6) NOT NULL DEFAULT '9999-12-31 23:59:59',
-    UNIQUE unq_data_id_name (data_id, name, deleted_at),
-    UNIQUE unq_bucket_id_name_version (bucket_id, name, version, deleted_at),
+    UNIQUE unq_name_version (name, version, deleted_at),
     CONSTRAINT object_region_fk FOREIGN KEY (region) REFERENCES regions (name) ON UPDATE CASCADE,
-    CONSTRAINT object_data_id_fk FOREIGN KEY (data_id) REFERENCES data (id) ON UPDATE CASCADE,
     CONSTRAINT object_bucket_id_fk FOREIGN KEY (bucket_id) REFERENCES buckets (id) ON UPDATE CASCADE
+) Engine=InnoDB CHARSET=utf8mb4;
+
+CREATE TABLE data (
+    id         INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id    VARCHAR(16) NOT NULL,
+    name       VARCHAR(32) NOT NULL,
+    data_type  VARCHAR(16) NOT NULL,
+    object_id  INT UNSIGNED NOT NULL,
+    created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    updated_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    deleted_at DATETIME(6) NOT NULL DEFAULT '9999-12-31 23:59:59',
+    UNIQUE unq_user_id_name (user_id, name, deleted_at),
+    CONSTRAINT data_type_fk FOREIGN KEY (data_type) REFERENCES data_types (name) ON UPDATE CASCADE,
+    CONSTRAINT data_object_id_fk FOREIGN KEY (object_id) REFERENCES objects (id) ON UPDATE CASCADE
 ) Engine=InnoDB CHARSET=utf8mb4;
